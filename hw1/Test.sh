dfb415d56d970ff4392785ec8326fcacc1af6092
shmuelamit <shmuelamit@mail.tau.ac.il>
2021-12-04 16:08:39 +0200

    Parallel tests to kill nova

--- hw1/Test.sh
+++ hw1/Test.sh
@@ -8,20 +8,27 @@ underline()     { ansi 4 "$@"; }
 strikethrough() { ansi 9 "$@"; }
 red()           { ansi 31 "$@"; }
 
+stop() {
+    kill $(jobs -p)
+}
+
+trap 'stop' SIGINT
+
 test() {
     if [ -f "./$1" ]; then
         echo $(underline "Running tests for $3!")
-        time (
-            for filename in tests/input_*; do   
+        for filename in tests/input_*; do   
+            (
                 out_diff=`$2 7 ${filename} /dev/stdout | diff - ${filename/in/out}`
                 if [ $? != 0 ]; then
-                    echo "There was a diffrence in $filename"
-                    echo "$out_diff"
+                    echo -e "There was a diffrence in $filename\n$out_diff"
                 else
                     echo "Finished test $filename"
                 fi
-            done
-        )
+            ) &
+        done
+
+        wait
     else
         echo "Cannot find file '$1', to run the $3 tests please include the executable."
         return -1;
